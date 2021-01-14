0bb9b7781c6ce6461b5be8192cae4145a1af923a
thallard <thallard@student.42lyon.fr>
2021-01-14 23:07:44 +0100

    Added --diff flag and help argument

--- test.sh
+++ test.sh
@@ -6,33 +6,41 @@
 #    By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/13 20:16:23 by thallard          #+#    #+#              #
-#    Updated: 2021/01/13 23:17:48 by thallard         ###   ########lyon.fr    #
+#    Updated: 2021/01/14 23:04:38 by thallard         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-GREEN='\033[1;32m'
+GREEN='\033[0;32m'
 RED='\033[0;31m'
 BLANK='\033[0m'
 YELLOW='\033[0;33m'
-cp ../minishell .
+BLUE='\033[0;34m'
+# cp ../minishell .
 
-# ECHO PART
-correct=0
+# Variables
 i=1
-cat tests/echo_tests.txt | while read line
+
+# Check if 0 arguments is set 
+if [ "$1" == "help" ]; then
+    printf "How to use this tester ?\n"
+    printf "    use ${YELLOW}\"bash test.sh [--diff] <name_command>\"${BLANK} to run a specific builtin command test (echo, unset, export, etc...).\n\n"
+    printf "    flag ${YELLOW}[--diff]${BLANK} allow when its enabled to see difference between your minishell results and real bash,
+    without this flag enabled you will only see if the test is correct.\n\n"
+    printf "    use ${YELLOW}\"bash test.sh all\"${BLANK} to run all commands test in the same time.\n"
+    exit
+fi
+# Built-in echo checker
+
+cat file_tests/echo_tests.txt | while read line
     do
         BASH_RESULT=$(echo $line "; exit" | bash 2>&-)
         BASH_EXIT=$?
         MINISHELL_RESULT=$(echo $line "; exit" | ./minishell 2>&-)
         MINISHELL_EXIT=$?
-        if [ "$line" == "Echo part without -n flag" ]; then
-            printf "${GREEN}$line\n\n" 
-        else
-            if [ "$BASH_RESULT" == "$MINISHELL_RESULT" ]; then
-                if [ "$BASH_EXIT" == "$MINISHELL_EXIT" ]; then
+        if [ "$1" == "--diff" ]; then
+            if [ "$BASH_RESULT" == "$MINISHELL_RESULT" ] && [ "$BASH_EXIT" == "$MINISHELL_EXIT" ]; then
                     printf "${GREEN}$i: $line\n"
-                    correct=$((correct + 1))
-                fi
+                    echo $line >> tmp/file
             else
                 if [ "$BASH_EXIT" == "$MINISHELL_EXIT" ]; then
                     printf "${RED}$i: [$line]\nbash: [$BASH_RESULT]${GREEN}[$BASH_EXIT]${RED} | minishell: [$MINISHELL_RESULT]${GREEN}[$MINISHELL_EXIT]\n"
@@ -40,7 +48,22 @@ cat tests/echo_tests.txt | while read line
                     printf "${RED}$i: [$line]\nbash: [$BASH_RESULT][$BASH_EXIT] | minishell: [$MINISHELL_RESULT][$MINISHELL_EXIT]\n"
                 fi
             fi
+        else
+            if [ "$BASH_RESULT" == "$MINISHELL_RESULT" ] && [ "$BASH_EXIT" == "$MINISHELL_EXIT" ]; then
+                printf "${GREEN}$i: [$line]\n"
+                echo $line >> tmp/file
+            else
+                printf "${RED}$i: [$line]\n"
+            fi
         fi
         i=$((i + 1))
     done
-printf "\n${GREEN}Conclusion : $correct/$i\n"
+
+if [ "$1" == "echo" ] || [ "$2" == "echo" ]; then
+    printf "\n${GREEN}Built-in echo result : $(cat tmp/file | wc -l | xargs)/$(cat file_tests/echo_tests.txt | wc -l | xargs) tests passed\n"
+    rm -rf tmp/file
+elif [ "$2" == "export" ]; then
+    printf "\n${GREEN}Built-in export result : $(cat tmp/file | wc -l | xargs)/$(cat file_tests/echo_tests.txt | wc -l | xargs) tests passed\n"
+    rm -rf tmp/file
+fi
+
